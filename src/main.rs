930db6107b4e26adc86fbfd76b28157d3d0cd488
Samuel Hicks <SamMHicks@hotmail.co.uk>
2020-10-25 22:26:54 +0000

    Prefixing tags with package names in workspaces

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,78 @@
 use std::borrow::Cow;
+use std::path::Path;
 use std::process::Command;
 
-fn main() {
-    let manifest_location = std::env::args()
-        .find(|arg| arg.strip_suffix("Cargo.toml").is_some())
-        .map_or(Cow::Borrowed("Cargo.toml"), Cow::Owned);
+#[derive(Debug, serde::Deserialize)]
+struct Package<'a> {
+    name: Cow<'a, str>,
+    id: Cow<'a, str>,
+    manifest_path: Cow<'a, Path>,
+}
 
-    let output = Command::new("git")
-        .arg("show")
-        .arg(manifest_location.as_ref())
+#[derive(Debug, serde::Deserialize)]
+struct Metadata<'a> {
+    #[serde(borrow)]
+    packages: Vec<Package<'a>>,
+    #[serde(borrow)]
+    workspace_members: Vec<Cow<'a, str>>,
+}
+
+fn main() {
+    let cargo_metadata = Command::new("cargo")
+        .arg("metadata")
+        .arg("--format-version")
+        .arg("1")
         .output()
         .unwrap();
 
-    if !output.status.success() {
-        panic!("Git show failed");
+    if !cargo_metadata.status.success() {
+        panic!("cargo metadata failed");
     }
 
-    let stdout = String::from_utf8(output.stdout).unwrap();
-    let new_version = stdout
-        .lines()
-        .skip_while(|line| !line.starts_with("@@"))
-        .skip(1)
-        .filter_map(|line| line.strip_prefix("+version = \"")?.strip_suffix("\""))
-        .next();
+    let metadata: Metadata = serde_json::from_slice(cargo_metadata.stdout.as_ref()).unwrap();
 
-    if let Some(new_version) = new_version {
-        let tag = format!("v{}", new_version);
-        println!("Tagging with {:?}", tag);
-        if !Command::new("git")
-            .arg("tag")
-            .arg(tag)
+    let workspace_packages = metadata
+        .packages
+        .iter()
+        .filter(|package| metadata.workspace_members.contains(&package.id))
+        .collect::<Vec<_>>();
+
+    for workspace_package in workspace_packages.iter() {
+        let git_show = Command::new("git")
+            .arg("show")
+            .arg(workspace_package.manifest_path.as_ref())
             .output()
-            .unwrap()
-            .status
-            .success()
-        {
-            panic!("Git tag failed");
-        };
+            .unwrap();
+
+        if !git_show.status.success() {
+            panic!("Git show failed");
+        }
+
+        let stdout = String::from_utf8(git_show.stdout).unwrap();
+        let new_version = stdout
+            .lines()
+            .skip_while(|line| !line.starts_with("@@"))
+            .skip(1)
+            .filter_map(|line| line.strip_prefix("+version = \"")?.strip_suffix("\""))
+            .next();
+
+        if let Some(new_version) = new_version {
+            let tag = if workspace_packages.len() == 1 {
+                format!("v{}", new_version)
+            } else {
+                format!("{}-v{}", workspace_package.name, new_version)
+            };
+            println!("Tagging {} with {:?}", workspace_package.name, tag);
+            if !Command::new("git")
+                .arg("tag")
+                .arg(tag)
+                .output()
+                .unwrap()
+                .status
+                .success()
+            {
+                panic!("Git tag failed");
+            };
+        }
     }
 }
