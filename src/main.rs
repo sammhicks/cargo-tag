82c170ee8da270ddca3fc69eac0fc020bbd64edf
Samuel Hicks <SamMHicks@hotmail.co.uk>
2020-10-25 14:53:39 +0000

    Implemented basic tagging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::borrow::Cow;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let manifest_location = std::env::args()
+        .find(|arg| arg.strip_suffix("Cargo.toml").is_some())
+        .map_or(Cow::Borrowed("Cargo.toml"), Cow::Owned);
+
+    let output = Command::new("git")
+        .arg("show")
+        .arg(manifest_location.as_ref())
+        .output()
+        .unwrap();
+
+    if !output.status.success() {
+        panic!("Git show failed");
+    }
+
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let new_version = stdout
+        .lines()
+        .skip_while(|line| !line.starts_with("@@"))
+        .skip(1)
+        .filter_map(|line| line.strip_prefix("+version = \"")?.strip_suffix("\""))
+        .next();
+
+    if let Some(new_version) = new_version {
+        let tag = format!("v{}", new_version);
+        println!("Tagging with {:?}", tag);
+        if !Command::new("git")
+            .arg("tag")
+            .arg(tag)
+            .output()
+            .unwrap()
+            .status
+            .success()
+        {
+            panic!("Git tag failed");
+        };
+    }
 }
